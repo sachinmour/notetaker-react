30a3dc7e22274d8b08df3c24198929c90f728225
Sachin Mour <rtr.sachinmour@gmail.com>
2016-03-30 16:38:18 +0000

    Added checks using proptypes

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -33,7 +33,7 @@ var Profile = React.createClass({
     return (
       <div className="row">
         <div className="col-md-4">
-          <UserProfile username={this.props.params.username} bio={this.state.bio} />
+          <UserProfile  bio={this.state.bio} />
         </div>
         <div className="col-md-4">
           <Repos username={this.props.params.username} repos={this.state.repos} />
